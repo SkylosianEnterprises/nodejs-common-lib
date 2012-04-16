a9a7a5606ced4b1ad8d64ba239ae5187b7ffc70f
dihnen <dihnen@5c144b29-6cdb-0741-a111-200b08c5c46f>
2012-04-16 21:58:43 +0000

    mkid and typo
    git-svn-id: http://subversion/svn/main/common-lib/trunk@35288 5c144b29-6cdb-0741-a111-200b08c5c46f

--- js/lib/common/model/member.js
+++ js/lib/common/model/member.js
@@ -1,6 +1,8 @@
 // member model
 // special methods to operate on a member
 
+var mkid = require('util/mkid').mkid;
+
 var memberUpdateTsKeys = ['last_update_timestamp'];
 var memberAddTsKeys = ['register_timestamp'];
 var memberLcTsKeys = ['lastname', 'firstname', 'email'];
@@ -43,7 +45,7 @@ var buildUpdate = exports.buildUpdate = function (data) {
 
 var buildAdd = exports.buildAdd = function (data) {
 	// Build the update query
-	var ip = member.register_ip || '0.0.0.0';
+	var ip = data.register_ip || '0.0.0.0';
 	data._id = mkid(ip);
 
 	return setLcValues(setAddTimeValues(setUpdateTimeValues(data)));
