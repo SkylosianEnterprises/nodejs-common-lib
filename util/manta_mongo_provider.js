ffaaa1b6a8d281cb4a9ef0fc77c1a390da25cdbb
David Ihnen <dihnen@manta.com>
2013-01-16 14:55:29 -0500

    numeric port

--- util/manta_mongo_provider.js
+++ util/manta_mongo_provider.js
@@ -7,14 +7,17 @@ exports.builder = function(c, f) {
 	var BSON = mongodb.BSONNative;
 
 	var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
-	var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? parseInt(process.env['MONGO_NODE_DRIVER_PORT']) : Connection.DEFAULT_PORT;
+	var port = parseInt(process.env['MONGO_NODE_DRIVER_PORT'] != null ? parseInt(process.env['MONGO_NODE_DRIVER_PORT']) : Connection.DEFAULT_PORT);
 
-	var Schema = mongoose.Schema;
-	var conschema = new Schema({ name: "mstest" });
-	var db = mongose.createConnection();
-	db.openSet('mongodb://'+host+':'+port+'/mstest', function (err, dbl) {
-		if (err) throw "MONGO CONNECT ERROR: " + err;
-		f(err, dbl);
+	var connectSchema = mongoose.Schema({ from: String, to: String, id: String, type: String })
+	var Connection = mongoose.model('Connection', connectSchema);
+	mongoose.connect('mongodb://'+host+':'+port+'/mstest');
+	var db = mongose.connection;
+	db.on('error', function (err) {
+		f(err, Connection);
 	} ):
-	db.model('Connection', conschema, 'mstest')
-}
+	db.once('open', function () {
+		f(null, dbl);
+	} );
+	Connection.find( { from: 'srj0gjgxrn9bn3' } );
+};
