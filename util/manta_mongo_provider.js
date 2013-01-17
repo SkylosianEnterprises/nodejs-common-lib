bd2ca498f614619575e681d9031e42338074adde
David Ihnen <dihnen@manta.com>
2013-01-17 16:56:39 -0500

    node

--- util/manta_mongo_provider.js
+++ util/manta_mongo_provider.js
@@ -4,6 +4,12 @@ exports.builder = function(c, f) {
 	var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
 	var port = parseInt(process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : 27017);
 
+	var connectArchiveSchema = mongoose.Schema(
+		{ from: { type: String, required: true }
+		, to: { type: String, required: true }
+		, id: { type: String, required: true }
+		, type: { type: String, required: true }
+		})
 	var connectSchema = mongoose.Schema(
 		{ from: { type: String, required: true }
 		, to: { type: String, required: true }
@@ -12,7 +18,7 @@ exports.builder = function(c, f) {
 		})
 	mongoose.connect('mongodb://'+host+':'+port+'/mstest');
 	var Connection = mongoose.model('connections', connectSchema);
-	var Connection_Archive = mongoose.model('connections_archive', connectSchema);
+	var Connection_Archive = mongoose.model('connections_archive', connectArchiveSchema);
 
 	var conn = mongoose.connection;
 	// This error handler for connect errors returns to the caller, since there
