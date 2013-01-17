5540d00dd7d6dae40cccdacc947940f917431522
David Ihnen <dihnen@manta.com>
2013-01-17 17:42:35 -0500

    node

--- util/manta_mongo_provider.js
+++ util/manta_mongo_provider.js
@@ -2,24 +2,23 @@ var mongoose = require('mongoose');
 var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
 var port = parseInt(process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : 27017);
 mongoose.connect('mongodb://'+host+':'+port+'/mstest');
+var connectArchiveSchema = mongoose.Schema(
+	{ from: { type: String, required: true }
+	, to: { type: String, required: true }
+	, id: { type: String, required: true }
+	, type: { type: String, required: true }
+	})
+var connectSchema = mongoose.Schema(
+	{ from: { type: String, required: true }
+	, to: { type: String, required: true }
+	, id: { type: String, required: true }
+	, type: { type: String, required: true }
+	})
+var Connection = mongoose.model('connections', connectSchema);
+var Connection_Archive = mongoose.model('connections_archive', connectArchiveSchema);
+//var Connection_Archive = mongoose.model('connections_archive', connectSchema);
 
 exports.builder = function(c, f) {
-
-	var connectArchiveSchema = mongoose.Schema(
-		{ from: { type: String, required: true }
-		, to: { type: String, required: true }
-		, id: { type: String, required: true }
-		, type: { type: String, required: true }
-		})
-	var connectSchema = mongoose.Schema(
-		{ from: { type: String, required: true }
-		, to: { type: String, required: true }
-		, id: { type: String, required: true }
-		, type: { type: String, required: true }
-		})
-	var Connection = mongoose.model('connections', connectSchema);
-	var Connection_Archive = mongoose.model('connections_archive', connectArchiveSchema);
-	//var Connection_Archive = mongoose.model('connections_archive', connectSchema);
 
 	var conn = mongoose.connection;
 	// This error handler for connect errors returns to the caller, since there
