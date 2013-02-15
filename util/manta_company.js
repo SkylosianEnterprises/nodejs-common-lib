1b3848a5386a28d96e2606421a4dc7b57ebf89c5
faktorsmak <rob.zakrzewski@gmail.com>
2013-02-15 17:45:43 -0500

    adding iso_country_cd^Co the company data we return

--- util/manta_company.js
+++ util/manta_company.js
@@ -65,8 +65,9 @@ MantaCompanyUtil.getCompanyDetailsLite = MantaCompanyUtil.prototype.getCompanyDe
 		var paramValues = Object.keys(companyIDs);
 		
 		// First look in manta_claims_processed to get the company info.  For any IDs we didn't find, try those from manta_contents_2
-		// This isn't the greatest solution, but the thinking is that this query will be replaced by a call to the "company service" once one exists.
-		client.query({name:'select claimed mids ' + params1.length, text: 'SELECT mid, company_name, city, statebrv, zip, phones0_number, hide_address FROM manta_claims_published WHERE mid IN (' + params1.join(',') + ')', values: paramValues}, function(err, results) {
+		// This isn't the greatest solution, but the thinking is that this query will be replaced by a call to the "company service" once one exist.
+		client.query({name:'select claimed mids ' + params1.length, text: 'SELECT mid, company_name, city, statebrv, zip, iso_country_cd, phones0_number, hide_address FROM manta_claims_published WHERE mid IN (' + params1.join(',') + ')', values: paramValues}, function(err, results) {
+			if (err) return callback(err);
 			var finalResults = {};
 			results.rows.forEach( function (row) {
 				finalResults[row.mid] = row;
@@ -83,9 +84,10 @@ MantaCompanyUtil.getCompanyDetailsLite = MantaCompanyUtil.prototype.getCompanyDe
 				getUnclaimed.then(function(client) {
 					client.query(
 						{ name: 'select unclaimed mids ' + params2.length
-						, text: 'SELECT mid, name1 as company_name, city, stabrv as statebrv, zip5 as zip, phone as phones0_number, 0 as hide_address FROM manta_contents_2 WHERE mid IN (' + params2.join(',') + ')'
+						, text: 'SELECT mid, name1 as company_name, city, stabrv as statebrv, zip5 as zip, iso_country_cd, phone as phones0_number, 0 as hide_address FROM manta_contents_2 WHERE mid IN (' + params2.join(',') + ')'
 						, values: unclaimedIDs
 						}, function(err, results) {
+							if (err) return callback(err);
 							results.rows.forEach(function(row) {
 								finalResults[row.mid] = row;
 							} );
